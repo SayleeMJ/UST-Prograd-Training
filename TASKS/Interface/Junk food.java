5108f1995fbf15dc84541454f1a08752fd270f69
Saylee Jagtap <jagtapsaylee.2020@gmail.com>
2021-11-28 18:17:14 +0530

    Class junk food code

---
+++ TASKS/Interface/Junk food.java
@@ -0,0 +1,9 @@
+public class JunkFood implements Food {
+     public void type() {
+        System.out.println("Junk food is unhealthy");
+    }
+     public void name() {
+        System.out.println("Burger");
+    }
+}
+
