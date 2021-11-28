504e03cdf1c2b9e5451f790fc1336d4fcdf8579c
Saylee Jagtap <jagtapsaylee.2020@gmail.com>
2021-11-28 14:31:10 +0530

    Main class tv

---
+++ TASKS/Abstract class/Tv.java
@@ -0,0 +1,13 @@
+public class Tv {
+    public static void main(String[] args) {
+        Television mi = new Mi();
+        Television ss = new Samsung();
+        mi.inches();
+        ss.inches();
+        ss.type();
+        ss.color();
+        mi.color();
+
+    }
+}
+
