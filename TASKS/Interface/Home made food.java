fad076bd467698b4f34d64d21423454b6a53ae50
Saylee Jagtap <jagtapsaylee.2020@gmail.com>
2021-11-28 18:13:45 +0530

    Class home-made food code

---
+++ TASKS/Interface/Home made food.java
@@ -0,0 +1,9 @@
+public class HomeMadeFood implements Food{
+    public void type() {
+        System.out.println("Home made food is healthy");
+    }
+    public void name() {
+        System.out.println("Roti and sabji");
+    }
+}
+
