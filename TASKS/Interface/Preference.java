26312418c150d773276db2d0fb71b0cb3889d8d0
Saylee Jagtap <jagtapsaylee.2020@gmail.com>
2021-11-28 18:24:06 +0530

    Main class preference

---
+++ TASKS/Interface/Preference.java
@@ -0,0 +1,11 @@
+public class Preference {
+    public static void main(String[] args) {
+        Food hmf = new HomeMadeFood();
+        Food jf = new JunkFood();
+        hmf.name();
+        hmf.type();
+        jf.name();
+        jf.type();
+    }
+}
+
