ef7f5d438fc16db60e806de9cba52aa4b890df50
Saylee Jagtap <jagtapsaylee.2020@gmail.com>
2021-12-01 16:59:03 +0530

    Solution for Tree Map

---
+++ DAY 8/TressMap.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class TreeMapExample {
+    public static void main(String[] args) {
+        TreeMap<Integer, String > map = new TreeMap<>();
+
+        map.put(100,"Saylee");
+        map.put(101,"Sanat");
+        map.put(102,"Sunita");
+        map.put(103,"Bhushan");
+        map.put(105,"Mohan");
+        map.put(104,"Nirmiti");
+        map.putIfAbsent(106, "Charndraknta");
+        System.out.println(map);
+        System.out.println("\nAfter invoking get method: " + map.get(101) + "\n");
+
+        Set set =map.entrySet();
+        for (Object o : set) {
+            Map.Entry<Integer, String> entry = (Map.Entry) o;
+            System.out.println(entry.getKey() + "->" + entry.getValue());
+        }
+
+        System.out.println("\nAfter invoking remove: " + map.remove(101,"Sanat"));
+        for (Map.Entry<Integer, String> m : map.entrySet()){
+            System.out.println(m.getKey() + "->" + m.getValue());
+        }
+
+        System.out.println("\nDescendingMap: "+map.descendingMap());
+        //Returns key-value pairs whose keys are less than or equal to the specified key.
+        System.out.println("\nHeadMap: "+map.headMap(102,true));
+        //Returns key-value pairs whose keys are greater than or equal to the specified key.
+        System.out.println("\nTailMap: "+map.tailMap(102,true));
+        //Returns key-value pairs exists in between the specified key.
+        System.out.println("\nSubMap: "+map.subMap(100, false, 102, true));
+
+        System.out.println("\nIs value present: " + map.containsValue(101));
+        System.out.println("\nIs value present: " + map.containsValue("Saylee"));
+    }
+}
+
