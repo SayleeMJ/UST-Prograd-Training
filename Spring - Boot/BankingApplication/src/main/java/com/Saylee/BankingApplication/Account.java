29b7405827d7fb1dba20f7812d59846fc2b5a5d9
Saylee Jagtap <94833327+SayleeMJ@users.noreply.github.com>
2021-12-27 15:39:04 +0530

    Update Account.java

--- Spring - Boot/BankingApplication/src/main/java/com/Saylee/BankingApplication/Account.java
+++ Spring - Boot/BankingApplication/src/main/java/com/Saylee/BankingApplication/Account.java
@@ -10,7 +10,6 @@ public class Account {
     double balance;
 
     Account() {
-        //balance=3000;
     }
 
     public double getBalance() {
@@ -34,46 +33,3 @@ public class Account {
         return balance;
     }
 }
-/*@Component
-public class Account {
-    int accountNumber;
-    String branch;
-    double balance;
-
-    Account() {
-        //balance=3000;
-    }
-
-//    public Account(String branch, int accountNumber, double balance) {
-//        this.branch = branch;
-//        this.accountNumber = accountNumber;
-//        this.balance = balance;
-//    }
-//
-//    public void setAccount(String branch, int accountNumber, double balance) {
-//        this.branch = branch;
-//        this.accountNumber = accountNumber;
-//        this.balance = balance;
-//    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public int getAccountNo() {
-        return accountNumber;
-    }
-    public double withdrawalAmount(double withdrawalAmount){
-        if (balance >= withdrawalAmount){
-            balance = balance-withdrawalAmount;
-        }
-        else {
-            System.out.println("Insufficient Balance");
-        }
-        return balance;
-    }
-    public double depositAmount(double depositAmount){
-        balance= balance+depositAmount;
-        return balance;
-    }
-}*/
